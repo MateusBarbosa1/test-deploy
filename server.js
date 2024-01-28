f0e30e29129c12eb01d7f365f998f86920c9c773
Mateus Barbosa <pg9779628@gmail.com>
2024-01-28 01:01:34 -0300

    deploy - ADD PAGES

--- server.js
+++ server.js
@@ -24,9 +24,16 @@ server.listen(port, host, (error) => {
 
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
+const bodyParser = require('body-parser');
 
 const prisma = new PrismaClient();
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, './views'));
 
 async function createUser(data) {
     return await prisma.users.create({ data });
@@ -36,16 +43,19 @@ async function readUsers() {
 }
 
 app.post('/create', async (req,res) => {
-    const userCreated = await createUser({
+    await createUser({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
     });
-    res.json(userCreated);
+    res.redirect('/read');
 });
 app.get('/read', async (req,res) => {
     const users = await readUsers();
-    res.json(users);
+    res.render('read', {users: users});
+});
+app.get('/create', (req,res) => {
+    res.render('create');
 });
 
 app.listen(process.env.PORT, () => { console.log('server running on port 3000!') });
